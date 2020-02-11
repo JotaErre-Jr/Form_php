80cffc55ae905bd62343860dc8325cfea0daa98f
Jotaerre <rochajotaerre@gmail.com>
2020-02-11 15:04:24 -0300

    concluido pagina repot.php

---
+++ public_html/report.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Aliens Abducted Me-Report an abduction</title>
+  </head>
+  <body>
+    <h2>Aliens Abducted Me-Report an abduction</h2>
+    <?php
+      $when_it_happened = $_POST['whenithappened'];
+      $how_long = $_POST['howlong'];
+      $alien_description = $_POST['aliendescription'];
+      $fang_spotted = $_POST['fangspotted'];
+      $email = $_POST['email'];
+
+      echo 'Thanks for submitting the form.<br/>';
+      echo 'You are abducted'.$when_it_happened.'<br/>';
+      echo 'ande ware gone for'.$how_long.'<br/>';
+      echo 'describe them:'.$alien_description.'<br/>';
+      echo 'Was fang there?'.$fang_spotted.'<br/>';
+      echo 'Your email eddress is'.$email;
+
+     ?>
+  </body>
+</html>
