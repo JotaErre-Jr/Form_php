86bccd898af2dc35411d039480889f40c5e3bc2a
Jotaerre <rochajotaerre@gmail.com>
2020-02-12 17:58:06 -0300

    concluda a configuração de envio de email, falta configurar o php para envio de e-mail, auterntiva phpMailer

--- public_html/report.php
+++ public_html/report.php
@@ -7,8 +7,7 @@
   <body>
     <h2>Aliens Abducted Me-Report an abduction</h2>
     <?php
-      $firt_name = $_POST['firtname'];
-      $last_name = $_POST['lastname'];
+      $name = $_POST['firtname'].' '.$_POST['lastname'];
       $when_it_happened = $_POST['whenithappened'];
       $how_long = $_POST['howlong'];
       $how_many = $_POST['howmany'];
@@ -16,16 +15,35 @@
       $alien_description = $_POST['aliendescription'];
       $fang_spotted = $_POST['fangspotted'];
       $email = $_POST['email'];
+      $other = $_POST['other'];
+
+      $msg = $name.' was abducted '.$when_it_happened.' and was gone for '.$how_long.'\n'.
+      'number of Aliens '.$how_many.'\n'.
+      'Alien description '.$alien_description.'\n'.
+      'What they did '.$what_they_did.'\n'.
+      'Fang spotted '.$fang_spotted.'\n'.
+      'Other comments'.$other.'\n';
+
+
+      $to = 'rochajotaerre@gmail.com';
+      $subject = 'Aliens abducted Me - abduction report';
+      $msg = "$name was abducted: $when_it_happened and was gone for: $how_long\n".
+      "number of Aliens: $how_many\n".
+      "Alien description: $alien_description\n".
+      "What they did: $what_they_did\n".
+      "Fang spotted: $fang_spotted\n".
+      "Other comments: $other\n";
+      mail($to, $subject, $msg, 'From'.$email);
 
       echo 'Thanks for submitting the form.<br/>';
-      echo 'My firt name is: '.$firt_name. '<br/>';
-      echo 'My second name is: '.$last_name. '<br/>';
+      echo 'MY Name is: '.$name. '<br/>';
       echo 'You are abducted '.$when_it_happened.'<br/>';
       echo 'Were:'.$how_many.'<br/>';
       echo 'ande ware gone for'.$how_long.'<br/>';
       echo 'describe them:'.$alien_description.'<br/>';
       echo 'Was fang there?'.$fang_spotted.'<br/>';
       echo 'Your email eddress is'.$email;
+      echo 'Other comments: '.$other;
 
      ?>
   </body>
