e827ac6a739d7d49537de977843d511cd7c76e99
JotaErre <rochajotaerre@gmail.com>
2020-02-12 22:20:06 -0300

    adicinada a bibliotec PHPMailer na past public_html

--- public_html/report.php
+++ public_html/report.php
@@ -44,6 +44,61 @@
       echo 'Was fang there?'.$fang_spotted.'<br/>';
       echo 'Your email eddress is'.$email;
       echo 'Other comments: '.$other;
+      /*
+      ############biblioteca PHPMailer################
+      */
+      // Caminho da biblioteca PHPMailer
+require 'PHPMailer-master/PHPMailerAutoload.php';
+
+// Instância do objeto PHPMailer
+$mail = new PHPMailer;
+
+// Configura para envio de e-mails usando SMTP
+$mail->isSMTP();
+
+// Servidor SMTP
+$mail->Host = 'smtp.gmail.com';
+
+// Usar autenticação SMTP
+$mail->SMTPAuth = true;
+
+// Usuário da conta
+$mail->Username = 'rochajotaerre@gmail.com';
+
+// Senha da conta
+$mail->Password = 'jmrj198623';
+
+// Tipo de encriptação que será usado na conexão SMTP
+$mail->SMTPSecure = 'ssl';
+
+// Porta do servidor SMTP
+$mail->Port = 465;
+
+// Informa se vamos enviar mensagens usando HTML
+$mail->IsHTML(true);
+
+// Email do Remetente
+$mail->From = 'rochajotaerre@gmail.com';
+
+// Nome do Remetente
+$mail->FromName = 'Jotaerre';
+
+// Endereço do e-mail do destinatário
+$mail->addAddress('jotaerre07@yahoo.com.br');
+
+// Assunto do e-mail
+$mail->Subject = 'E-mail PHPMailer';
+
+// Mensagem que vai no corpo do e-mail
+$mail->Body = '<h1>Mensagem enviada via PHPMailer</h1>';
+
+// Envia o e-mail e captura o sucesso ou erro
+if($mail->Send()):
+    echo 'Enviado com sucesso !';
+else:
+    echo 'Erro ao enviar Email:' . $mail->ErrorInfo;
+endif;
+
 
      ?>
   </body>
